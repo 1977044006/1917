15a637d94fb2edad95736a8fd7337156f8991f2b
742671129 <742671129@qq.com>
2019-12-26 17:48:04 +0800

    dxx-xiugai

--- js/x-login.js
+++ js/x-login.js
@@ -1,65 +1,17 @@
 var form=document.querySelector('form');
+var ipts=document.querySelectorAll('form input')
 var user=document.querySelector('.user');
 var pass=document.querySelector('.pass');
 var erroalert=document.querySelector('.x-erro-alert-write');
 var errouser=document.querySelector('.x-erro-alert-user');
 var erropass=document.querySelector('.x-erro-alert-pass');
+var errouser2=document.querySelector('.x-erro-alert-user2');
+var erropass2=document.querySelector('.x-erro-alert-pass2');
+
 
 //点击from提交时判断
-var form = document.querySelector('form');
-var ipts = document.querySelectorAll('.row input');
-var tips = document.querySelectorAll('.row span');
-var result = [];//保存没有一个输入框的验证结果
-
-function verify(x,reg){
-    ipts[x].onblur = function (){//失去焦点时验证
-        var val = ipts[x].value.replace(/\s+/g,'');
-        // var reg = /^.{2,24}$/g;
-        if (reg.test(val)) {
-            result[x] = 1;//标记验证成功
-        } else {
-            result[x] = 0;//标记验证失败
-        }
-    }
-}
-
-var reg1 = /^\w{8,16}$/g;
-var reg2 = /^[0-9a-zA-Z]{8,16}$/g;
-var reg3 = /^\w{4,22}@[a-z0-9]{1,12}(\.[a-z]{2,3}){1,2}$/g;
-var reg4 = /^1[3-9]\d{9}$/g;
-var reg5 = /^男|女$/g;
-var reg6 = /^.{2,24}$/g;
-var regArr = [reg1,reg2,reg3,reg4,reg5,reg6];
-
-for (var i = 0; i < ipts.length; i++){
-    verify(i,regArr[i]);
-}
-
-// [1,1,1,0,1,1]
-
-form.onsubmit = function (){//表单提交的时候触发此事件
-    // 空值判断
-    if (!ipts[0].value || !ipts[1].value || !ipts[2].value || !ipts[3].value || !ipts[4].value || !ipts[5].value) {
-        alert('输入不能为空');
-        return false;
-    }
-
-    // 验证每一个输入框是否合法
-    for (var index in result){
-        if (result[index] == 0) {
-            alert('请正确输入！');
-            return false;
-        }
-    }
-
-    // 允许提交
-    alert('注册成功！');
-}
-
-
-
 form.onsubmit=function(){
-    //账号和密码提交为空时
+    //账号和密码都提交为空时
     if (!user.value && !pass.value) {
         erroalert.style.display='block';
         pass.style.border='1px solid #ff6700';
@@ -88,17 +40,45 @@ form.onsubmit=function(){
     var reg3=/^[0-9a-zA-Z_]{6,12}$/g;  //6到12个字符，可以是数字、字母(区分大小写),下划线
 
     //判断账号框
+    //分别验证两个输入框为错，出现的情况
+    //用一个变量保存两个分别为对和错的情况
+    //判断变量最后两个是否为对
+
+    var result=[]; //保存一个空数组
+    //判断账号
     if (reg.test(userval)||reg2.test(userval)) {
-        alert('格式正确！');
+        result.push(1);  //账号对了push1
     } else {
+        errouser2.style.display='block';
+        user.style.border='1px solid #ff6700';
+        result.push(0);  //账号错了pish0
         return false;
     }
 
     //判断密码框
     if (reg3.test(passval)) {
-        alert('格式正确！');
+        result.push(1);  //账号对了push1
     } else {
+        erropass2.style.display='block';
+        result.push(0);  //账号错了push0
         return false;
     }
 
+    // 验证每一个输入框是否合法
+    for (var index in result){
+        if (result[index] == 0) {
+            alert('请正确输入账号密码！');
+            return false;
+        }
+    }
+
+    setCookie('username',user.value,7);
+    setCookie('userpass',pass.value,7);
 }
+
+$(function() { 
+    if (getCookie('username')) {
+        user.value = getCookie('username');
+        pass.value = getCookie('userpass');
+    }　
+});
